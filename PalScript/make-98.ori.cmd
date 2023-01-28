39741ec88b58209cccd89782f7b179836c6e0a57
palxex <ex@palx.org>
2023-01-29 03:27:29 +0800

    bugfix: makes PalScript/*.cmds works

--- PalScript/make-98.ori.cmd
+++ PalScript/make-98.ori.cmd
@@ -1,5 +1,5 @@
 copy /y sss-mkf-98-ori sss.mkf
 copy /y m-msg-98-ori m.msg
 copy /y word-dat-98-ori word.dat
-cut sss.mkf bin
+call cut sss.mkf bin
 script > 98.ori.txt
--- PalScript/make-original.cmd
+++ PalScript/make-original.cmd
@@ -1,5 +1,5 @@
 copy /y sss-mkf-original sss.mkf
 copy /y m-msg-original m.msg
 copy /y word-dat-original word.dat
-cut sss.mkf bin
+call cut sss.mkf bin
 script > dos.original.txt
