3b4f9ae06592eb383363857c63b0368f3de9335f
palxex <ex+home@palx.org>
2024-07-25 02:22:41 +0800

    chore: IDB update refactor: segment updater adopted to python3

--- EXE/txts/updatesegs.py
+++ EXE/txts/updatesegs.py
@@ -1,23 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import io
 import os
 
-lines=io.open("segments.txt","rt").read().split(os.linesep)
-outf=io.open("tmp","wt",newline=os.linesep)
+lines=[]
+with open("segments.txt","rt") as file:
+	lines=file.readlines()
 
 # calc offset base on segment 3; since it contains entrypoint
 seg3info=lines[4].split('\t')
 offset=int(seg3info[2],16)-int(seg3info[1],16)
-offhex="%x"%offset
 
-outf.write(lines[0]+os.linesep)
+outlines=[]
+outlines.append(lines[0])
 for segn in range(1,len(lines)): 
 	seginfo=lines[segn].split('\t')
 	if len(seginfo)<3:
 		continue
-	seginfo[2]="%x"%(int(seginfo[1],16)+offset)
+	seginfo[2]="%04X"%(int(seginfo[1],16)+offset)
 	segline='\t'.join(seginfo)
-	outf.write(segline+os.linesep)
+	outlines.append(segline)
 
-os.rename("tmp", "segments.txt")
+with open("segments.txt","wt") as outf:
+	outf.writelines(outlines)
