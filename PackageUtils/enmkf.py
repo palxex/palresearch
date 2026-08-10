14ca025d5c7da22a2a032835b49a10d1bdcdaa01
palxex <ex@palx.org>
2026-08-11 05:20:41 +0800

    feat(packageutils): add option for packing midi.mkf from win resource to enmkf

--- PackageUtils/enmkf.py
+++ PackageUtils/enmkf.py
@@ -5,16 +5,27 @@ import struct
 import os
 import re
 
+altScheme = False
+verbose = False
+
 def getname(prefix,postfix,i):
+    if altScheme:
+        return "{:03d}".format(i)+"."+postfix
     return prefix+str(i)+"."+postfix
 
 def enMKF(prefix,postfix):
     maxfiles=10000
+    if verbose:
+        print("finding maximum number of files...")
     for i in range(0,maxfiles):
         filename=getname(prefix,postfix,i)
+        if verbose:
+            print("checking file: "+filename)
         if not os.path.isfile(filename):
             maxfiles=i
             break
+    if verbose:
+        print("maximum number of files found: "+str(maxfiles))
     indexes=struct.pack("<I",(maxfiles+1)*4)
     offset=(maxfiles+1)*4
     for i in range(0,maxfiles):
@@ -24,7 +35,7 @@ def enMKF(prefix,postfix):
     with open(prefix+".mkf", 'wb') as mkffile:
         mkffile.write(indexes)
         for i in range(0,maxfiles):
-            with open(prefix+str(i)+"."+postfix, 'rb') as subfile:
+            with open(getname(prefix,postfix,i), 'rb') as subfile:
                 mkffile.write(subfile.read())
 
 if __name__ == "__main__":
@@ -33,6 +44,12 @@ if __name__ == "__main__":
                        help='prefix for files to pack')
     parser.add_argument('--postfix', required=True,
                        help='postfix for files to pack')
+    parser.add_argument('--altscheme', action='store_true', default=False,
+                          help='use alternative naming scheme to find file(mainly for repacking midi from win98 resource): no prefix, 3 digit index')
+    parser.add_argument('--verbose', action='store_true', default=False,
+                          help='enable verbose output')
 
     args = parser.parse_args()
+    altScheme = args.altscheme
+    verbose = args.verbose
     enMKF(args.prefix, args.postfix)
